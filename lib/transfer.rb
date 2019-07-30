fdfc36dba39a7a5fa29a59a0335485bc04c14b23
Josh Morris <jmo0624@gmail.com>
2019-07-30 01:37:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,9 @@
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
   
-  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 
 end
